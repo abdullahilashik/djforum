3056bce1a854c98b484d181db2aeb08e754fca36
Abdullahil Ashik Md. Arefin <abdullah.ashik.arefin@gmail.com>
2021-11-21 23:36:27 +0600

    topic

--- forums/urls.py
+++ forums/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     QuestionListView,
-    QuestionDetailsView
+    QuestionDetailsView,
+    TopicListview
 )
 
 app_name = 'forums'
 urlpatterns = [
-    path('', QuestionListView.as_view(), name='question-list'),
+    path('', TopicListview.as_view(), name='topic-list'),
+    path('questions/<slug:topic>/', QuestionListView.as_view(), name='question-list'),
     path('<slug:slug>/', QuestionDetailsView.as_view(), name='question-slug')
 ]
--- forums/views.py
+++ forums/views.py
@@ -5,8 +5,12 @@ from django.views import generic
 
 class QuestionListView(generic.ListView):
     template_name = 'forum/question-list.html'
-    queryset = Question.objects.all()
+    # queryset = Question.objects.all()
     context_object_name = 'questions'
+
+    def get_queryset(self):
+        queryset = Question.objects.filter(topic__slug=self.kwargs['topic'])
+        return queryset
 
 
 class QuestionDetailsView(generic.DetailView):
@@ -14,3 +18,9 @@ class QuestionDetailsView(generic.DetailView):
     slug_field = 'slug'
     queryset = Question.objects.all()
     context_object_name = 'question'
+
+
+class TopicListview(generic.ListView):
+    template_name = 'forum/topic-list.html'
+    queryset = Topic.objects.all()
+    context_object_name = 'topics'
